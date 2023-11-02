776a4336533074aad3e22cc9deab5e720937f323
alikadev <dev@alikadev.com>
2023-11-02 21:55:28 +0100

    Complete the task and add debug informations

--- task1/src/main.c
+++ task1/src/main.c
@@ -1,69 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <errno.h>
 
 #include "app.h"
 
 extern int errno;
 
+extern robot_t robot;
+
 int main(int argc, char const *argv[])
 {
-	app_error error;
+	app_status status;
 	app_args args;
 	char *buffer = 0;
+	char *start;
 
 
 	// === Get the arguments === //
-	error = app_get_args(&args, argc, argv);
+	status = app_get_args(&args, argc, argv);
 
-	if (error == APP_NOT_ENOUTH_ARGS
-		|| error == APP_TOO_MUCH_ARGS) 
+	if (status == APP_NOT_ENOUTH_ARGS
+		|| status == APP_TOO_MUCH_ARGS) 
 	{
 		printf("Usage: %s inputFile\n", *argv);
 		exit(0);
 	}
 
-	if (error != APP_NO_ERROR)
+	if (status != APP_SUCCESS)
 	{
-		fprintf(stderr, "Unexpected error (0x%X)\n", error);
+		fprintf(stderr, "Unexpected error (0x%X)\n", status);
 		goto exit_failure;
 	}
 
+	debugf("Input file: %s\n", args.inputFile);
 
 	// === Read the file === //
-	error = app_read_all(&buffer, args.inputFile);
+	status = app_read_all(&buffer, args.inputFile);
+	start = buffer;
 
-	if (error == APP_FILE_OPEN_ERROR)
+	if (status == APP_FILE_OPEN_ERROR)
 	{
-		printf("Fail to open the file '%s': %s\n", args.inputFile, strerror(errno));
+		fprintf(stderr, "Fail to open the file '%s': %s\n", 
+				args.inputFile, strerror(errno));
 		goto exit_failure;
 	}
 
-	if (error == APP_ALLOC_ERROR)
+	if (status == APP_ALLOC_ERROR)
 	{
-		printf("Fail allocate memory: %s\n", strerror(errno));
+		fprintf(stderr, "Fail allocate memory: %s\n", 
+				strerror(errno));
 		goto exit_failure;
 	}
 
-	if (error == APP_READ_ERROR)
+	if (status == APP_READ_ERROR)
 	{
-		printf("Fail read the file '%s': %s\n", args.inputFile, strerror(errno));
+		fprintf(stderr, "Fail read the file '%s': %s\n", 
+				args.inputFile, strerror(errno));
 		goto exit_failure;
 	}
 
+	// === Prepare the world === // 
+	status = app_simulation_init(&buffer);
+	if (status == APP_BAD_FILE_FORMAT)
+	{
+		fprintf(stderr, "Bad file format...\n");
+		goto exit_failure;
+	}
 
-	printf("File content:\n%s\n", buffer);
+	status = app_simulate(buffer);
+	if (status == APP_BAD_INSTRUCTION)
+	{
+		fprintf(stderr, "Bad instruction\n");
+		goto exit_failure;
+	}
 
+	printf("Report: %ld %ld %c\n", robot.x, robot.y, robot.direction);
 
 	// === The End === //
-	free(buffer);
+	free(start);
 	return 0;
 
 	// This part of the code free all the possibly 
 	// allocated bytes and exit as failure.
 	exit_failure:
-	if (buffer)
-		free(buffer);
+	if (start)
+		free(start);
 	return 1;
 }
